e4c0f6e6326c464e9a663fba3aeee7028de23df9
BitpodSaurabh <saurabh@bitpod.io>
2018-01-25 13:01:10 +0530

    logo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import logo from "./assets/logo.png";
 const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
+  backgroundColor:'#3b78e7'
 };
 
 const App = () => (
   <div style={styles}>
-    <h2>Hello bitpod</h2>
+  <img src={logo}/>
+    <h2>Welcome to bitpod</h2>
+    <h2>React site webdeploy</h2>
   </div>
 );
 
